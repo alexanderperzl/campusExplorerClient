240b0209d58d229da160f0aeec543602fdcd3929
Peter Ehrich <p.ehrich@campus.lmu.de>
2019-01-27 20:41:04 +0100

    new building mapping

--- app/src/main/java/com/example/campusexplorer/BuildingIDConverter.kt
+++ app/src/main/java/com/example/campusexplorer/BuildingIDConverter.kt
@@ -4,7 +4,16 @@ object BuildingIDConverter {
     // TODO expand this to use more buildings
     private var mapping: HashMap<String, String> = hashMapOf(
         "bw0000" to "Geschw",
-        "bw7070" to "Oetting"
+        "bw7070" to "Oetting",
+        "bw0420" to  "Prof.-Huber-Pl. 2",
+        "bw0200" to  "Schellingstr. 3",
+        "bw0073" to "Amalienstr. 52",
+        "bw0147" to "Amalienstr. 73A",
+        "bw0050" to "Schellingstr. 4",
+        "bw1020" to   "Amalienstr. 17",
+        "bw0250" to "Schellingstr. 9",
+        "bw1003" to "Theresienstr. 39"
+
     )
 
     fun fromClientToServer(id: String): String? {
